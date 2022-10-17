9cd8e4a7f53861f320bc4be54c302fa6a8221e4f
Anil Shrestha <anilmeronaam@gmail.com>
2022-10-17 19:22:43 +1100

    Update firebase.js
    resetting the configuration file

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,13 @@ import  { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABKVob9PneOA6rpZT7W8fxXhgjm3_OxGc",
-  authDomain: "eventme-2022.firebaseapp.com",
-  projectId: "eventme-2022",
-  storageBucket: "eventme-2022.appspot.com",
-  messagingSenderId: "514139029631",
-  appId: "1:514139029631:web:bd6a554daef484f09b3f4a",
-  measurementId: "G-85SE99GVW5"
+  apiKey: "---Your API Key---",
+  authDomain: "---Your App Domain---",
+  projectId: "---Your ProjectId---",
+  storageBucket: ".....",
+  messagingSenderId: "....",
+  appId: ".....",
+  measurementId: "....."
 };
 
 // Initialize Firebase
